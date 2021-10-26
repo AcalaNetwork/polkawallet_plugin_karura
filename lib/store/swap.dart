6bed781ee35d364025f186408bd81023a43fc422
dingzuhua <dingzuhua@qq.com>
2021-10-26 11:02:29 +0800

    swap remote mobx

--- lib/store/swap.dart
+++ lib/store/swap.dart
@@ -1,23 +1,14 @@
 import 'dart:convert';
 
-import 'package:mobx/mobx.dart';
 import 'package:polkawallet_plugin_karura/store/cache/storeCache.dart';
 
-part 'swap.g.dart';
-
-class SwapStore extends _SwapStore with _$SwapStore {
-  SwapStore(StoreCache cache) : super(cache);
-}
-
-abstract class _SwapStore with Store {
-  _SwapStore(this.cache);
+class SwapStore {
+  SwapStore(this.cache);
 
   final StoreCache cache;
 
-  @observable
   Map<String, List<String>> _swapPair = Map<String, List<String>>();
 
-  @action
   void setSwapPair(List<String> value, String pubKey) {
     _swapPair[pubKey] = value;
     if (value != cache.swapPair.val[pubKey]) {
@@ -27,12 +18,10 @@ abstract class _SwapStore with Store {
     }
   }
 
-  @action
   List<String> swapPair(String pubKey) {
     return _swapPair[pubKey] ?? [];
   }
 
-  @action
   void loadCache(String pubKey) {
     if (pubKey == null || pubKey.isEmpty) return;
 
