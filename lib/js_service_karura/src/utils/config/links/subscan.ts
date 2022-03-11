3c6c6b003a9aa53d727d366a052d1e57013de6fa
shawn <dhryxy@gmail.com>
2022-03-11 12:18:26 +0800

    update gov links

--- lib/js_service_karura/src/utils/config/links/subscan.ts
+++ lib/js_service_karura/src/utils/config/links/subscan.ts
@@ -1,14 +1,9 @@
 export default {
   chains: {
-    "Acala Mandala TC4": "acala-testnet",
-    Kusama: "kusama",
-    "Kusama CC3": "kusama",
-    Polkadot: "polkadot",
-    "Polkadot CC1": "polkadot-cc1",
-    Westend: "westend",
+    Acala: "acala",
+    Karura: "karura",
   },
-  create: (chain: string, path: string, data: any) =>
-    `https://${chain}.subscan.io/${path}/${data.toString()}`,
+  create: (chain: string, path: string, data: any) => `https://${chain}.subscan.io/${path}/${data.toString()}`,
   isActive: true,
   paths: {
     address: "account",
