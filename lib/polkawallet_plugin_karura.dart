81b3dd657f0c5cefb969d1bc1a576527a0c0e93b
shawn <dhryxy@gmail.com>
2022-04-19 19:06:44 +0800

    bump jsCodeVersion

--- lib/polkawallet_plugin_karura.dart
+++ lib/polkawallet_plugin_karura.dart
@@ -66,7 +66,7 @@ class PluginKarura extends PolkawalletPlugin {
           isTestNet: name != plugin_name_karura,
           isXCMSupport: name == plugin_name_karura,
           parachainId: '2000',
-          jsCodeVersion: 31901,
+          jsCodeVersion: 32001,
         );
 
   @override
