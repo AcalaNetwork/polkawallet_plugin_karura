630068274f0a0e861777c0cf1090baf3887215bf
shawn <dhryxy@gmail.com>
2023-02-06 16:07:43 +0800

    move warningRow into UI repo

--- lib/common/components/insufficientKARWarn.dart
+++ lib/common/components/insufficientKARWarn.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:polkawallet_plugin_karura/common/components/warningRow.dart';
 import 'package:polkawallet_plugin_karura/utils/i18n/index.dart';
 import 'package:polkawallet_sdk/utils/i18n.dart';
+import 'package:polkawallet_ui/components/v3/warningRow.dart';
 
 class InsufficientKARWarn extends StatelessWidget {
   @override
